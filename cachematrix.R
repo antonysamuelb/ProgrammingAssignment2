829b55208b031731ec3288468c019740b080356d
AntonySamuelB <antonysamuelb@gmail.com>
2017-04-27 01:10:30 +0530

    assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,60 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Code to invert a matrix in a fast manner 
 
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+## function to create list of functions that can
+## get and set the matrix as well as its inverse
+
+#x1 - matrix
+#x2 - inverse of matrix
+
+makeCacheMatrix <- function(x1 = matrix()) {
+    
+    
+    x2 <- NULL
+    set <- function(matrx) {
+ 
+        x1 <<- matrx
+        x2 <<- NULL
+    }
+
+    
+    get <- function() x1
+    setinv <- function(matinv) x2 <<- matinv
+    getinv <- function() x2
+    
+    
+    
+    list(set = set, get = get,
+         setinv = setinv,
+         getinv = getinv)
+    
+    
 
 }
 
 
-## Write a short comment describing this function
+## solve to obtain inverse of a matrix if inverse has not been calculated. 
+## If it has been calculated, load that value.
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+
+cacheSolve <- function(x,  ...) {
+    
+    matrx <- matrix(c(4,2:9),nrow=3,ncol=3)
+    
+    matrixold <- x$get()
+    print(matrixold)
+    if(!identical(matrx,matrixold)){
+        x$set(matrx)
+    }
+    
+    matinv <- x$getinv()
+    if(!is.null(matinv)) {
+
+        message("getting matrix inverse from cached data")
+        return(matinv)
+    }
+    matrx <- x$get()
+    matinv <- solve(matrx)
+    x$setinv(matinv)
+    
 }
